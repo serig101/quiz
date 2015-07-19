e2911aeb24cbf03c764904194654a5321d2ac44b
serig101 <serig101@yahoo.es>
2015-07-19 13:17:18 +0200

    Crear Preguntas

--- routes/index.js
+++ routes/index.js
@@ -17,8 +17,10 @@ router.get('/author', function(req, res) {
 router.param('quizId', quizcontroller.load);
 
 // Definicion de rutas de /quizes
-router.get('/quizes',quizcontroller.index);
-router.get('/quizes/:quizId(\\d+)',quizcontroller.show);
-router.get('/quizes/:quizId(\\d+)/answer',quizcontroller.answer);
+router.get('/quizes', quizcontroller.index);
+router.get('/quizes/:quizId(\\d+)', quizcontroller.show);
+router.get('/quizes/:quizId(\\d+)/answer', quizcontroller.answer);
+router.get('/quizes/new', quizcontroller.new);
+router.post('/quizes/create', quizcontroller.create);
 
 module.exports = router;
